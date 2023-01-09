79dedde41b816629c364c472e6426d7c0890cf4a
Ianto Lin Xi <iantolinxi@gmail.com>
2023-01-08 22:31:16 -0500

    create tally

--- call-draft/src/components/window.js
+++ call-draft/src/components/window.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 import Calendar from './calendar'
+import Tally from './tally'
 import List from './list'
 
 import styles from './window.module.css'
 
 const Window = () => <div className={styles.window}>
-  <Calendar/>
+  <div>
+    <Calendar/>
+    <Tally/>
+  </div>
   <List/>
 </div>
 
